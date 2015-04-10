e12ba9f8c5ce5e9447be437e319cf4a7bc1300eb
ZhiHao Qian <lacusrinz@gmail.com>
2015-04-10 10:31:45 +0800

    release 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'beecloud',
-    version = '1.2',
+    version = '1.2.1',
       
     keywords = ('beecloud', 'pay'),
     description = 'beecloud, make pay more simple',
