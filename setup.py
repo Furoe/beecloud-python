a7e8156d1d0e93ea863b0d705d7a354205c973eb
ZhiHao Qian <lacusrinz@gmail.com>
2015-04-17 15:51:50 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'beecloud',
-    version = ‘1.2.3’,
+    version = '1.2.3',
       
     keywords = ('beecloud', 'pay'),
     description = 'beecloud, make pay more simple',
